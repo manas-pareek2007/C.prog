1c0c7ea4e87b81482c17be4a6d7aa329b20df75b
manas-pareek2007 <manaspareek34@gmail.com>
2024-10-22 22:23:08 +0530

    Add files via upload

---
+++ SUMOFNNO.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,n;
+clrscr();
+printf("enter the no.\n ");
+scanf("%d",&n);
+s=(n*(n+1))/2;
+   printf("sum of no. =is %d",s);
+   getch();
+   }
